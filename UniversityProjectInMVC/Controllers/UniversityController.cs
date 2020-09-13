3066097db7fd5ee98b43415d96e16b85ae72de2c
Juniad <AhmadKhoonee@gmail.com>
2020-09-13 02:06:14 -0700

    UnversityProject

---
+++ UniversityProjectInMVC/Controllers/UniversityController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using UniversityProjectInMVC.Models;
+
+namespace UniversityProjectInMVC.Controllers
+{
+    public class UniversityController : Controller
+    {
+
+        public Contxt db = new Contxt();
+
+        public ActionResult Index(string search)
+        {
+            if (search != null)
+            {
+               return View(db.faculties.Where(x => x.Name.Contains(search)).ToList());
+            }
+
+           return View(db.faculties.ToList());
+        }
+
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Create(Faculty f)
+        {
+            db.faculties.Add(f);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+    }
+}
---
+++ UniversityProjectInMVC/Models/Contxt.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace UniversityProjectInMVC.Models
+{
+    public class Contxt : DbContext
+    { 
+            public Contxt() : base("Contxt")
+            {
+
+            }
+            public DbSet<Faculty> faculties { get; set; }
+        }
+
+    }
